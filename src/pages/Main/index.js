080ac5e92860a26ca207ed7fad7617b0997bda8d
Arli Machado <arli.machado@b2wdigital.com>
2019-05-08 16:59:05 -0300

    Finalizando a aplicação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.png';
 import CompareList from '../../components/CompareList/index';
 
 import { Container, Form } from './styles';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
 
-export default Main;
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositories, repositoryInput, repositoryError, loading,
+    } = this.state;
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">{loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
